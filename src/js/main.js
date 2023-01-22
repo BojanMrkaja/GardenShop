2401182078ad856b4c345ef4398e70611fbd00d5
Bojan Mrkaja <39374674+BojanMrkaja@users.noreply.github.com>
2023-01-22 18:14:21 +0100

    Header mobile done

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,29 @@
 
-console.log('Boris');
+const navToggler = document.querySelector('.navigation-toggler');
+
+const searchMobileIcon = document.querySelector('.header__search-mobile');
+const searchPopup = document.querySelector('.search-popup');
+const searchPopupBtn = document.querySelector('.search-popup__btn');
+const searchPopupInput = document.querySelector('.search-popup__input');
+const searchPopupClose = document.querySelector('.search-popup__close');
+
+//Hamburger menu animation
+navToggler.addEventListener('click', function (){
+    navToggler.classList.toggle('active');
+});
+
+//Search mobile
+searchMobileIcon.addEventListener('click', function (){
+    searchPopup.style.opacity = '1';
+    searchPopup.style.visibility = 'visible';
+    searchPopupInput.classList.add('animation-1');
+    searchPopupBtn.classList.add('animation-2');
+});
+
+searchPopupClose.addEventListener('click', function (){
+    searchPopup.style.opacity = '0';
+    searchPopup.style.visibility = 'hidden';
+    searchPopupInput.classList.remove('animation-1');
+    searchPopupBtn.classList.remove('animation-2');
+})
+
