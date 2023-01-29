a7cc5aadd237726b7fb19bfdb8502d7cecda9e1d
Bojan Mrkaja <39374674+BojanMrkaja@users.noreply.github.com>
2023-01-29 14:24:39 +0100

    header and navigation done

--- src/js/main.js
+++ src/js/main.js
@@ -7,9 +7,15 @@ const searchPopupBtn = document.querySelector('.search-popup__btn');
 const searchPopupInput = document.querySelector('.search-popup__input');
 const searchPopupClose = document.querySelector('.search-popup__close');
 
+const navigation = document.querySelector('.navigation');
+const navigationDropdownLinks = [...document.querySelectorAll('[data-dropdown]')];
+const dropdown = document.querySelectorAll('.navigation__dropdown');
+
 //Hamburger menu animation
 navToggler.addEventListener('click', function (){
-    navToggler.classList.toggle('active');
+    this.classList.toggle('active');
+
+    navigation.classList.toggle('show');
 });
 
 //Search mobile
@@ -25,5 +31,11 @@ searchPopupClose.addEventListener('click', function (){
     searchPopup.style.visibility = 'hidden';
     searchPopupInput.classList.remove('animation-1');
     searchPopupBtn.classList.remove('animation-2');
-})
+});
+
+navigationDropdownLinks.forEach(dropdownLink => {
+    dropdownLink.addEventListener('click', function (){
+        this.nextElementSibling.classList.toggle('show');
+    });
+});
 
